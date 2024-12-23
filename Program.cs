e83b6078efe4597e6ae3dc57faefefcc37a43a08
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2024-12-23 11:06:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+class ChickenAndEgg
+{
+    static readonly object lockObject = new object();
+    static bool eggSaidLast = false;
+
+    public static void ChickenThread()
+    {
+        lock (lockObject)
+        {
+            Console.WriteLine("Курица");
+            eggSaidLast = false;
+        }
+    }
+
+    public static void EggThread()
+    {
+        lock (lockObject)
+        {
+            Console.WriteLine("Яйцо");
+            eggSaidLast = true;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Thread chickenThread = new Thread(ChickenThread);
+        Thread eggThread = new Thread(EggThread);
+
+        chickenThread.Start();
+        eggThread.Start();
+
+        chickenThread.Join();
+        eggThread.Join();
+
+        if (eggSaidLast)
+        {
+            Console.WriteLine("Ответ: Сначала появилось яйцо.");
+        }
+        else
+        {
+            Console.WriteLine("Ответ: Сначала пояявилась курица.");
+        }
+    }
+}
